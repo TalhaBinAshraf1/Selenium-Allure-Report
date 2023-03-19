fc7bb12aaca15602fdc065d19a45856d3140d968
TalhaBinAshraf1 <talhabinashraf@gmail.com>
2023-03-19 13:07:48 +0600

    selenium

---
+++ selenium/demoProject/herokuapp/src/test/java/com/internet/herokuapp/utilities/Data.java
@@ -0,0 +1,9 @@
+package com.internet.herokuapp.utilities;
+
+public class Data {
+
+    public static final String BASE_URL = "http://the-internet.herokuapp.com/";
+    public static final String VALID_URL = "URL is Valid";
+    public static final String AB_TESTING = "A/B Testing";
+
+}
